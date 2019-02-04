1570f2fb78acf8a327ba8912625376084acf04f9
Qinmei Du <duqinmei@bu.edu>
2019-02-04 15:50:34 -0500

    Create UserInterface_module.py

---
+++ UserInterface_module.py
@@ -0,0 +1,18 @@
+#It's the I/O documentation for the User_Interface_system
+
+#User Interface
+def userinterface_input():
+	"""
+	Get_data_from_data_base:
+		format:(double value, int type)
+	Get_data_from_alert_sys:
+		format: three flags from alert sys output to trigger alert display
+	Get_data_from_user:
+	    format: boolean control from user. Such as turn on, turn off
+	            user log in information
+	"""
+
+def userinterface_output():
+    """
+    send input to electron device to show on the screen
+    """
